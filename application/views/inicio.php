e7e050e48b6a09e8622bf89b6da510fabcf1d22a
Jorge Gabriel <jperez.471@jpit.com.mx>
2021-02-23 20:02:18 -0600

    :tada: Initial commit

---
+++ application/views/inicio.php
@@ -0,0 +1,64 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+require (VIEWPATH.'includes/header.php');
+?>
+
+    <section class="d-flex justify-content-center pt-3" style="background-color: #eee;">
+        <div class="d-flex justify-content-between w-75 p-2 pl-4 pr-4 border rounded" style="background-color: white;">
+                <div class="h-100 d-flex align-items-center pt-1">
+                    <h4>Visita nuestra nueva línea</h4>
+                </div>
+                <div class="d-flex align-items-center">
+                    <div class="h-100 d-flex align-items-center pt-1 mr-5">
+                        <h5>Celulares con nanoteconlogía</h5>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus ml-4" viewBox="0 0 16 16">
+                        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                    </svg>
+                </div>
+        </div>
+    </section>
+
+    <main class="d-flex justify-content-center pt-3 pb-3" style="background-color: #eee;">
+        <div class="d-flex flex-column w-75 p-5 border rounded" style="background-color: white;">
+            <div class="d-flex justify-content-center">
+                <h1>CELULARES E-COMMERCE DEL MUNDO PARA TI</h1>
+            </div>
+            <div class="d-flex justify-content-center p-3 pl-4 pr-4">
+                <p>
+                    Ut euismod, ipsum a ornare luctus, odio ex laoreet eros, nec pretium lectus dui sed massa. Praesent vitae neque tristique ex auctor commodo ut vitae justo. In hac habitasse platea dictumst. Aenean id dictum mi, quis egestas nulla. Ut gravida, ipsum vitae finibus tincidunt, augue neque venenatis orci, faucibus eleifend ante nisi eu dolor. Cras convallis ut augue sed porttitor. Vestibulum quis nulla orci. Cras consequat ipsum nisl, nec egestas nulla elementum et.
+
+                    Nulla ut massa dignissim velit efficitur vulputate. Fusce fermentum non nibh non cursus. Praesent bibendum maximus neque eget aliquam. Proin ut tempor metus. Phasellus tempor euismod convallis. Donec feugiat euismod nisi a molestie. Cras mollis tortor in nunc semper placerat at ac ante. Morbi eu elit eget est venenatis finibus id sit amet magna. Phasellus lorem leo, consequat ut libero eu, suscipit dapibus magna. Donec lacinia pharetra rhoncus. Aliquam et est tempus, fermentum elit eu, fermentum erat. Ut ac diam in tellus ultrices placerat eget quis tellus. Fusce vel ipsum tincidunt libero eleifend venenatis vitae ac elit.
+                </p>
+            </div>
+            <div class="d-flex justify-content-center mb-5">
+                <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-file-earmark-image-fill p-2" viewBox="0 0 16 16">
+                    <path d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707v5.586l-2.73-2.73a1 1 0 0 0-1.52.127l-1.889 2.644-1.769-1.062a1 1 0 0 0-1.222.15L2 12.292V2a2 2 0 0 1 2-2zm5.5 1.5v2a1 1 0 0 0 1 1h2l-3-3zm-1.498 4a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z"/>
+                    <path d="M10.564 8.27L14 11.708V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-.293l3.578-3.577 2.56 1.536 2.426-3.395z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-file-earmark-image-fill p-2" viewBox="0 0 16 16">
+                    <path d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707v5.586l-2.73-2.73a1 1 0 0 0-1.52.127l-1.889 2.644-1.769-1.062a1 1 0 0 0-1.222.15L2 12.292V2a2 2 0 0 1 2-2zm5.5 1.5v2a1 1 0 0 0 1 1h2l-3-3zm-1.498 4a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z"/>
+                    <path d="M10.564 8.27L14 11.708V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-.293l3.578-3.577 2.56 1.536 2.426-3.395z"/>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-file-earmark-image-fill p-2" viewBox="0 0 16 16">
+                    <path d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707v5.586l-2.73-2.73a1 1 0 0 0-1.52.127l-1.889 2.644-1.769-1.062a1 1 0 0 0-1.222.15L2 12.292V2a2 2 0 0 1 2-2zm5.5 1.5v2a1 1 0 0 0 1 1h2l-3-3zm-1.498 4a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z"/>
+                    <path d="M10.564 8.27L14 11.708V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-.293l3.578-3.577 2.56 1.536 2.426-3.395z"/>
+                </svg>
+            </div>
+            <div class="d-flex justify-content-center">
+				<a href="<?php echo site_url('productos/index')?>">
+					<button class="btn btn-outline-dark" style="border-radius: 50px; height: 4rem; width: 10rem">
+						Ver más
+						<span class="ml-2">
+							<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+								<path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+							</svg>
+						</span>
+					</button>
+				</a>
+            </div>
+        </div>
+    </main>
+</body>
+
+<?php include('includes/footer.php') ?>
+
