af8d3289acdcf5d4d56f5a25c799888f9d11d06c
Jorge Gabriel <jperez.471@jpit.com.mx>
2021-02-23 20:24:37 -0600

    :tada: initial commit

--- application/controllers/Inicio.php
+++ application/controllers/Inicio.php
@@ -3,7 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Inicio extends CI_Controller
 {
-	public function index() {
-        $this->load->view('inicio');
-    }
+	public function index()
+	{
+		$this->load->view('inicio');
+	}
 }
